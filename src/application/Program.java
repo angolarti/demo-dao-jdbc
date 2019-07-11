7c61bc922a113e42b003c247602e8877af3f6595
Walter ANGOLAR <angolarti@gmail.com>
2019-07-11 10:38:16 +0100

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,22 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Department department = new Department(1, "Books");
-		System.out.println(department);
+		
+		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(sdf.parse("22/03/1987").getTime()), 
+				3000.0, department);
+
+		System.out.println(seller);
 	}
 }
